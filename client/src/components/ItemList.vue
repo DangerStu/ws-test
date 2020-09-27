a90456acc71941db1c8a650391e21b18ce5ccf15
Stuart Gregg <sgregg@CommonAreas.com>
2020-09-27 16:41:05 -0700

    WIP

--- client/src/components/ItemList.vue
+++ client/src/components/ItemList.vue
@@ -1,75 +1,84 @@
 <template>
-	<div>
-		<b-navbar id="nav-bar" toggleable="lg" type="light" :sticky="true">
-			<img class="logo-image" src="https://assets.wsimgs.com/wsimgs/ab/images/i/202039/0014/images/common/logo.svg" alt="Williams Sonoma" />
+  <div id="app">
+    <b-navbar
+      id="nav-bar"
+      toggleable="lg"
+      type="light"
+      :sticky="true"
+      class="justify-content-center"
+    >
+      <img
+        class="logo-image"
+        src="https://assets.wsimgs.com/wsimgs/ab/images/i/202039/0014/images/common/logo.svg"
+        alt="Williams Sonoma"
+      />
+    </b-navbar>
 
-			<b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
-			<b-collapse id="nav-collapse" is-nav>
-				<!-- Right aligned nav items -->
-				<b-navbar-nav class="ml-auto">
-					<b-nav-form class="mt-4 mt-lg-0">
-						<b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
-						<b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
-					</b-nav-form>
-				</b-navbar-nav>
-			</b-collapse>
-		</b-navbar>
-
-		<div class="container">
-			<div class="row">
-				<div v-for="item in items" :key="item.id" class="col-12 col-md-6 col-lg-4 ">
-					<Item :item="item"></Item>
-				</div>
-			</div>
-		</div>
-		<div>
-			<ItemModal :item="selectedItem" :modelVisible="modelVisible" @changeVisible="setModalVisible" />
-		</div>
-	</div>
+    <div class="container">
+      <div class="row">
+        <div
+          v-for="item in items"
+          :key="item.id"
+          class="col-12 col-md-6 col-lg-4"
+        >
+          <Item :item="item"></Item>
+        </div>
+      </div>
+    </div>
+    <div>
+      <ItemModal
+        :item="selectedItem"
+        :modelVisible="modelVisible"
+        @changeVisible="setModalVisible"
+      />
+    </div>
+  </div>
 </template>
 
 <script>
-	import Item from "./Item";
-	import ItemModal from "./ItemModal";
+import Item from "./Item";
+import ItemModal from "./ItemModal";
 
-	export default {
-		name: "ItemList",
-		components: { Item, ItemModal }, // eslint-disable-line
-		data: () => ({
-			items: [],
-			selectedItem: null,
-			modelVisible: false,
-		}),
-		computed: {
-			selectedItemName() {
-				return this.selectedItem ? this.selectedItem.name : "";
-			},
-		},
-		methods: {
-			showModal(item) {
-				this.selectedItem = item;
-				this.setModalVisible(true);
-			},
-			hideModal() {
-				this.setModalVisible(false);
-			},
-			setModalVisible(value) {
-				this.modelVisible = value;
-			},
-		},
-		created() {
-			this.items = window.items.groups;
-		},
-		provide: function() {
-			return { showModal: this.showModal, hideModal: this.hideModal };
-		},
-	};
+export default {
+  name: "ItemList",
+  components: { Item, ItemModal }, // eslint-disable-line
+  props: {
+    items: Array,
+  },
+  data: () => ({
+    // items: [],
+    selectedItem: null,
+    modelVisible: false,
+  }),
+  computed: {
+    selectedItemName() {
+      return this.selectedItem ? this.selectedItem.name : "";
+    },
+  },
+  methods: {
+    showModal(item) {
+      this.selectedItem = item;
+      this.setModalVisible(true);
+    },
+    hideModal() {
+      this.setModalVisible(false);
+    },
+    setModalVisible(value) {
+      this.modelVisible = value;
+    },
+  },
+  // created() {
+  //   this.items = window.items.groups;
+  // },
+  provide: function () {
+    return { showModal: this.showModal, hideModal: this.hideModal };
+  },
+};
 </script>
 
 <style lang="sass">
-	.navbar
-		background-color: var(--white);
-		box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+.navbar
+	background-color: var(--white);
+	box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
 </style>
 <style></style>
--- client/src/components/ItemModal.vue
+++ client/src/components/ItemModal.vue
@@ -1,122 +1,171 @@
 <template>
-	<b-modal id="modal-details" ref="modal-details" role="dialog" hide-footer centered size="md" ok-variant="secondary">
-		<template v-slot:modal-header>
-			<div class="container">
-				<div class="row">
-					<div class="col col-10 px-0 card-title mb-0" v-html="item.name"></div>
-					<!-- <h5>Modal Header</h5> -->
-					<div class="col col-2">
-						<button type="button" class="close" aria-label="Close" @click="hideModal()">×</button>
-					</div>
-				</div>
-				<div class="row">
-					<div class="col px-0 card-text small">
-						<strong>{{ regularPrice }}</strong>
-					</div>
-				</div>
-				<div class="row">
-					<div class="col px-0 card-text text-danger small">
-						<strong>{{ salePrice }}</strong>
-					</div>
-				</div>
-			</div>
-		</template>
-		<div class="row justify-content-center mb-6 ">
-			<div v-if="item" class="mx-2 pb-5" style="background-color: lightslategray;">
-				<b-carousel
-					id="carousel-1"
-					v-model="slide"
-					:interval="4000"
-					:controls="showControls"
-					:indicators="showControls"
-					background="#323232"
-					:fade="true"
-					style="text-shadow: 1px 1px 2px #333;max-width:363px;"
-					@sliding-start="onSlideStart"
-					@sliding-end="onSlideEnd"
-				>
-					<b-carousel-slide :key="item.hero.href" :img-src="item.hero.href"> </b-carousel-slide>
+  <b-modal
+    id="modal-details"
+    ref="modal-details"
+    role="dialog"
+    hide-footer
+    centered
+    size="md"
+    ok-variant="secondary"
+    v-model="modelVisible"
+  >
+    <template v-slot:modal-header>
+      <div class="container">
+        <div class="row">
+          <div class="col col-10 px-0 card-title mb-0" v-html="item.name"></div>
 
-					<b-carousel-slide v-for="image in item.images" :key="image.href" :img-src="image.href"> </b-carousel-slide>
-				</b-carousel>
-			</div>
-
-			<!-- <div class="row">
-			<p class="mt-4">
-				Slide #: {{ slide }}<br />
-				Sliding: {{ sliding }}
-			</p>
-		</div> -->
-		</div>
-	</b-modal>
+          <div class="col col-2">
+            <button
+              type="button"
+              class="close"
+              aria-label="Close"
+              @click="hideModal()"
+            >
+              ×
+            </button>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col px-0 card-text small">
+            <strong>{{ regularPrice }}</strong>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col px-0 card-text text-danger small">
+            <strong>{{ salePrice }}</strong>
+          </div>
+        </div>
+      </div>
+    </template>
+    <div class="container">
+      <div class="row justify-content-center mb-6">
+        <div v-if="item" class="mx-2">
+          <b-carousel
+            id="carousel-1"
+            v-model="slide"
+            :interval="4000"
+            :controls="showControls"
+            :indicators="false"
+            background="#323232"
+            :fade="true"
+            style="text-shadow: 1px 1px 2px #333; max-width: 363px"
+            @sliding-start="onSlideStart"
+            @sliding-end="onSlideEnd"
+          >
+            <b-carousel-slide
+              v-for="(image, index) in allImages"
+              :key="`${index}-${image.href}`"
+              :img-src="image.href"
+            >
+            </b-carousel-slide>
+          </b-carousel>
+        </div>
+      </div>
+      <div
+        v-if="item && item.images.length > 0"
+        class="row justify-content-center my-3"
+      >
+        <div
+          v-for="(image, index) in allImages"
+          :key="`${index}-${image.href}`"
+          class="col-3"
+        >
+          <img
+            @click="selectSlide(index)"
+            :src="image.href"
+            :class="{ thumbs: true, 'thumbs-hilight': index === slide }"
+          />
+        </div>
+      </div>
+    </div>
+  </b-modal>
 </template>
 
 <script>
-	import { priceMixin } from "./mixins/price";
-	export default {
-		name: "ItemModal",
+import { priceMixin } from "./mixins/price";
+export default {
+  name: "ItemModal",
 
-		props: {
-			item: Object,
-			modelVisible: Boolean,
-		},
+  props: {
+    item: Object,
+    modelVisible: Boolean,
+  },
 
-		mixins: [priceMixin],
+  mixins: [priceMixin],
 
-		watch: {
-			modelVisible(visible) {
-				if (visible) {
-					this.$refs["modal-details"].show();
-				} else {
-					this.$refs["modal-details"].hide();
-				}
-			},
-		},
+  data: () => ({
+    slide: 0, // eslint-disable-line
+    sliding: null,
+  }),
 
-		data: () => ({
-			slide: 0, // eslint-disable-line
-			sliding: null,
-		}),
-		computed: {
-			showControls() {
-				return this.item ? this.item.images.length > 0 : false;
-			},
-			regularPrice() {
-				return this.fullFormatPrice("Regular Price:", this.item, "regular", "$");
-			},
-			salePrice() {
-				return this.fullFormatPrice("Our Price:", this.item, "selling", "$");
-			},
-		},
-		methods: {
-			onSlideStart() {
-				this.sliding = true;
-			},
-			onSlideEnd() {
-				this.sliding = false;
-			},
-			showModal() {
-				this.$emit("changeVisible", true);
-			},
-			hideModal() {
-				this.$emit("changeVisible", false);
-			},
-		},
-	};
+  computed: {
+    showControls() {
+      return this.item ? this.item.images.length > 0 : false;
+    },
+    regularPrice() {
+      return this.fullFormatPrice("Regular Price:", this.item, "regular");
+    },
+    salePrice() {
+      return this.fullFormatPrice("Our Price:", this.item, "selling");
+    },
+    allImages() {
+      if (this.item) {
+        return [this.item.hero, ...this.item.images];
+      }
+      return [];
+    },
+  },
+
+  methods: {
+    onSlideStart() {
+      this.sliding = true;
+    },
+    onSlideEnd() {
+      this.sliding = false;
+    },
+    showModal() {
+      this.$emit("changeVisible", true);
+    },
+    hideModal() {
+      this.$emit("changeVisible", false);
+    },
+    selectSlide(slideId) {
+      this.slide = slideId;
+    },
+  },
+
+  mounted() {
+    this.$root.$on("bv::modal::show", (bvEvent, modalId) => {
+      if (modalId === "modal-details") {
+        this.slide = 0;
+      }
+    });
+  },
+};
 </script>
 
 <style lang="sass">
-	.carousel-control-next,
-	.carousel-control-prev
-		filter: invert(100%);
+.carousel-control-next,
+.carousel-control-prev
+	filter: invert(100%);
 
-	.carousel-indicators
-		bottom: -50px !important;
-		li
-			width: 10px !important;
-			height: 10px !important;
-			border-radius: 100%;
-			background-color: black !important;
-			// padding: 0 5px;
-			margin: 0 7px !important;
+.carousel-indicators
+	bottom: -50px !important;
+	li
+		width: 10px !important;
+		height: 10px !important;
+		border-radius: 100%;
+		background-color: black !important;
+		// padding: 0 5px;
+		margin: 0 7px !important;
+
+.thumbs
+	width: 100%;
+	border: solid 2px #eeeeee 
+	border-radius: 4px
+	padding: 2px;
+	margin: 4px auto;
+	transition: border-color 0.5s ease;
+.thumbs-hilight
+	border-color: #bbb
 </style>
